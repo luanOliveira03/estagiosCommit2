8ed4f180c2d9df80e7215b82bcb50ff5c0c30ad4
luanOliveira03 <luan00534@gmail.com>
2022-10-27 18:58:39 -0300

    commit da main 1

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const somaUmMaisUm = () => 1 + 1
 
 const multiplicaDoisVezesUm = () => 2 * 1
 
+const main = () => {
+
 console.log(somaUmMaisUm())
 
 console.log(multiplicaDoisVezesUm())
+
+}
+
+main()
